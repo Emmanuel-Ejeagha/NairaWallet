33ce46406fa8953bcdd8aae2d54734494f9c8b73
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-02-24 14:09:08 +0100

    chore(domain): add global using directives for domain layer

---
+++ backend/NairaWallet.Domain/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using NairaWallet.Domain.Entities;
+global using NairaWallet.Domain.Exceptions;
+global using NairaWallet.Domain.Enums;
+global using NairaWallet.Domain.ValueObjects;
+global using NairaWallet.Domain.Interfaces;
+
