8cd6a84a6e305287bb19dc5163b71eb06b32340f
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-08 14:06:02 +0100

    feat(application): add core application service interfaces

--- backend/NairaWallet.Application/NairaWallet.Application/Common/Interfaces/Repositories/IUserRepository.cs
+++ backend/NairaWallet.Application/NairaWallet.Application/Common/Interfaces/Repositories/IUserRepository.cs
@@ -3,7 +3,7 @@
 public interface IUserRepository
 {
     Task<User?> GetByIdAsync(UserId id, CancellationToken cancellationToken = default);
-    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<User?> GetByEmailAsync(Email email, CancellationToken cancellationToken = default);
     Task<User?> GetByWalletTagAsync(WalletTag Tag, CancellationToken cancellationToken = default);
     Task<bool> ExistsByEmailAsync(Email email, CancellationToken cancellationToken = default);
     void Add(User user);
