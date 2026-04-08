8a03fe552f12050c0342179e5e4a2e5a551018d4
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-08 14:04:44 +0100

    feat(application): add common exceptions for forbidden, not found, and validation errors

---
+++ backend/NairaWallet.Application/NairaWallet.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace NairaWallet.Application.Common.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string entityName, object key)
+        : base($"Entity '{entityName}' ({key}) was not found")
+    {
+        
+    }
+}
