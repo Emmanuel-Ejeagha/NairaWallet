a403b46f8d4d5e77acc906d27d224c8ce4507839
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-08 14:07:48 +0100

    feat(application): add DTOs for transaction, user, and wallet

---
+++ backend/NairaWallet.Application/NairaWallet.Application/DTOs/TransactionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace NairaWallet.Application.DTOs;
+
+public class TransactionDto
+{
+    public string Id { get; init; } = string.Empty;
+    public string Reference { get; init; } = string.Empty;
+    public decimal Amount { get; init; }
+    public string Type { get; init; } = string.Empty;
+    public string Status { get; init; } = string.Empty;
+    public string Descripotion { get; init; } = string.Empty;
+    public DateTime CreatedAtUtc { get; init; }
+    public string? ReversalTransactionId { get; init; }
+    public string? ReversedTransactionId { get; init; }
+}
