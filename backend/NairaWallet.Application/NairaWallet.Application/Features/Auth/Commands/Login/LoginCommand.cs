91ee6594dcf18f631abcec6399112ac6dc223861
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-09 21:05:36 +0100

    feat(application): implement Login command, validator and handler

---
+++ backend/NairaWallet.Application/NairaWallet.Application/Features/Auth/Commands/Login/LoginCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace NairaWallet.Application.Features.Auth.Commands.Login;
+
+public record LoginCommand : IRequest<LoginResponse>
+{
+    public string Email { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
+
+public record LoginResponse
+{
+    public string Token { get; init; } = string.Empty;
+    public string RefreshToken { get; init; } = string.Empty;
+    public UserDto User { get; init; } = null!;
+}
