5cce1cba3f5f357b041edb16ee31aae119960ce2
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-09 21:09:19 +0100

    feat(application): implement Transfer command, validator, and handler

---
+++ backend/NairaWallet.Application/NairaWallet.Application/Features/Wallet/Commands/Transfer/TransferCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace NairaWallet.Application.Features.Wallet.Commands.Transfer;
+
+public record TransferCommand : IRequest<TransactionDto>
+{
+    public string FromWalletTag { get; init; } = string.Empty;
+    public string ToWalletTag { get; init; } = string.Empty;
+    public decimal Amount { get; init; }
+    public string Description { get; init; } = string.Empty;
+    public string IdempotencyKey { get; init; } = string.Empty;
+}
