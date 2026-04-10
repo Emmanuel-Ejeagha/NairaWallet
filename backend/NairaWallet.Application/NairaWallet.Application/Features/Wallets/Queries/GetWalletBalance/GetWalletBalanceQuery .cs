e9c6cfd3b09a6d48a650891997c040f1c9db3514
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-10 15:58:37 +0100

    feat(application): implement GetWalletBalance query, validator and handler

---
+++ backend/NairaWallet.Application/NairaWallet.Application/Features/Wallets/Queries/GetWalletBalance/GetWalletBalanceQuery .cs
@@ -0,0 +1,6 @@
+﻿namespace NairaWallet.Application.Features.Wallets.Queries.GetWalletBalance;
+
+public record GetWalletBalanceQuery : IRequest<WalletDto>
+{
+    public string WalletTag { get; init; } = string.Empty;
+}
---
+++ backend/NairaWallet.Application/NairaWallet.Application/Features/Wallets/Queries/GetWalletBalance/GetWalletBalanceQueryValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace NairaWallet.Application.Features.Wallets.Queries.GetWalletBalance;
+
+public class GetWalletBalanceQueryValidator : AbstractValidator<GetWalletBalanceQuery>
+{
+    public GetWalletBalanceQueryValidator()
+    {
+        RuleFor(x => x.WalletTag)
+            .NotEmpty().Must(WalletTag.IsValid).WithMessage("Invalid wallet tag format.");
+    }
+}
