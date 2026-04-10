e498e188cd68e2f5f39323fc1cad7b91708076af
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-10 15:49:47 +0100

    feat(application): add more parameters to GetByWalletIdAsync signature

--- backend/NairaWallet.Application/NairaWallet.Application/Common/Interfaces/Repositories/ITransactionRepository.cs
+++ backend/NairaWallet.Application/NairaWallet.Application/Common/Interfaces/Repositories/ITransactionRepository.cs
@@ -4,7 +4,7 @@ public interface ITransactionRepository
 {
     Task<Transaction?> GetByIdAsync(TransactionId id, CancellationToken cancellationToken = default);
     Task<Transaction?> GetByReferenceAsync(TransactionReference reference, CancellationToken cancellationToken = default);
-    Task<IEnumerable<Transaction>> GetByWalletIdAsync(WalletId walletId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Transaction>> GetByWalletIdAsync(WalletId walletId, int page, int pagesize, CancellationToken cancellationToken = default);
     void Add(Transaction transaction);
     void Update(Transaction transaction);
 }
