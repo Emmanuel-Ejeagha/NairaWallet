00f94529dd2d8eff91a317ba9b3e5b69dd95b4a6
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-10 17:38:09 +0100

    feat(infrastructure): add ApplicationDbContext implementation

---
+++ backend/NairaWallet.Infrastructure/Persistence/ApplicationDbContext .cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace NairaWallet.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    private IDbContextTransaction? _currentTransaction;
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Wallet> Wallets => Set<Wallet>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+    public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
+    public DbSet<IdempotencyRecord> IdempotencyRecords => Set<IdempotencyRecord >();
+    public DbSet<FraudFlag> FraudFlags => Set<FraudFlag>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)    {  }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_currentTransaction != null)
+            return;
+        _currentTransaction = await Database.BeginTransactionAsync(System.Data.IsolationLevel.Serializable, cancellationToken);
+    }
+
+    public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_currentTransaction == null)
+            throw new InvalidOperationException("No active transaction to commit.");
+        try
+        {
+            await SaveChangesAsync(cancellationToken);
+            await (_currentTransaction.CommitAsync(cancellationToken) ?? Task.CompletedTask);
+        }
+        catch
+        {
+            await RollbackTransactionAsync(cancellationToken);
+            throw;
+        }
+        finally
+        {
+            _currentTransaction?.Dispose();
+            _currentTransaction = null;
+        }
+    }
+
+    public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_currentTransaction == null)
+            return;
+        try
+        {
+            await (_currentTransaction.RollbackAsync(cancellationToken) ?? Task.CompletedTask);
+        }
+        finally
+        {
+            _currentTransaction?.Dispose();
+            _currentTransaction = null;
+        }
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
