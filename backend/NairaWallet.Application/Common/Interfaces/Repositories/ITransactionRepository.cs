86b41021bd9f082d75c80eaf1e1b4f24fad0f811
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-16 23:28:55 +0100

    feat(application): add repository interfaces for audit, fraud, idempotency, transaction, and user

---
+++ backend/NairaWallet.Application/Common/Interfaces/Repositories/ITransactionRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace NairaWallet.Application.Common.Interfaces.Repositories;
+
+public interface ITransactionRepository
+{
+    Task<Transaction?> GetByIdAsync(TransactionId id, CancellationToken cancellationToken = default);
+    Task<Transaction?> GetByReferenceAsync(TransactionReference reference, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Transaction>> GetByWalletIdAsync(WalletId walletId, int page, int pagesize, CancellationToken cancellationToken = default);
+    void Add(Transaction transaction);
+    void Update(Transaction transaction);
+}
---
+++ backend/NairaWallet.Application/Common/Interfaces/Repositories/IWalletRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace NairaWallet.Application.Common.Interfaces.Repositories;
+
+public interface IWalletRepository
+{
+    Task<Wallet?> GetByIdAsync(WalletId id, CancellationToken cancellationToken = default);
+    Task<Wallet?> GetByUserIdAsync(UserId userId, CancellationToken cancellationToken = default);
+    Task<Wallet?> GetByTagAsync(WalletTag tag, CancellationToken cancellationToken = default);
+    void Add(Wallet wallet);
+    void Update(Wallet wallet);
+}
